0d5a4a24721afb93ab43339a5dede7eb613ca6cc
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2025-11-30 21:44:32 +0100

    [ADD] start game transition

--- components/start_menu/StartMenu.cs
+++ components/start_menu/StartMenu.cs
@@ -17,6 +17,7 @@ public partial class StartMenu : Panel
     private Tween _colorTween;
 
     private bool _isReadyToStart = false;
+    private bool _isTransitioning = false;
 
     public override void _Ready()
     {
@@ -58,10 +59,44 @@ public partial class StartMenu : Panel
 
     public override void _Input(InputEvent @event)
     {
-        if (!_isReadyToStart) return;
+        if (!_isReadyToStart || _isTransitioning) return;
         bool isTap = @event is InputEventScreenTouch touchEvent && touchEvent.Pressed;
-		if (!isTap) return;
-        GetTree().ChangeSceneToPacked(_gameScene);
+        if (!isTap) return;
+        
+        _isTransitioning = true;
+        TransitionToGame();
         AcceptEvent();
+    }
+
+    private async void TransitionToGame()
+    {
+        var viewport = GetViewport();
+        var viewportSize = viewport.GetVisibleRect().Size;
+        
+        var gameScene = _gameScene.Instantiate<Node>();
+        GetTree().Root.AddChild(gameScene);
+        
+        if (gameScene is Control gameControl)
+        {
+            gameControl.Position = new Vector2(0, -viewportSize.Y);
+        }
+        
+        var tween = GetTree().CreateTween();
+        tween.SetParallel(true);
+        
+        tween.TweenProperty(this, "position", new Vector2(0, viewportSize.Y), 0.5f)
+            .SetTrans(Tween.TransitionType.Quad)
+            .SetEase(Tween.EaseType.InOut);
+        
+        if (gameScene is Control gc)
+        {
+            tween.TweenProperty(gc, "position", Vector2.Zero, 0.5f)
+                .SetTrans(Tween.TransitionType.Quad)
+                .SetEase(Tween.EaseType.InOut);
+        }
+        
+        await ToSignal(tween, Tween.SignalName.Finished);
+        
+        QueueFree();
     }
 }
